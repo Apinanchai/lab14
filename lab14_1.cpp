96b7a03972b468e899015a91b37bedd0cf029fb3
Chanan <Apinanchai0411@gmail.com>
2023-01-13 16:05:39 +0700

    lab14_1

--- lab14_1.cpp
+++ lab14_1.cpp
@@ -17,3 +17,36 @@ int main(){
     cout << "\nMin = " << B[5];
     return 0;
 }
+
+void stat(const double A[],int N,double B[]){
+
+    B[4] = A[0],B[5] =A[0];
+    for(int i=1;i<N;i++){
+        if(A[i]>B[4]) B[4]=A[i];
+        if(A[i]<B[5]) B[5]=A[i];
+    }
+
+    double b;
+    double d;
+    double c =1;
+
+    for(int i=0;i<N;i++){
+        B[0] += A[i]/N;
+    }
+
+    for(int i=0;i<N;i++){
+        b += pow(A[i],2);
+        B[1] = sqrt((b/N)-pow(B[0],2));
+    }
+
+    for(int i=0;i<N;i++){
+        c *= A[i];
+        B[2] = pow(c,1.0/N);
+    }
+
+    for(int i=0;i<N;i++){
+        d += 1/A[i];
+        B[3] = N/d;
+
+    }
+}
